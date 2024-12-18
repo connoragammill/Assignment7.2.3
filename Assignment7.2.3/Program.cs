8bf06aaf961e6cb5a4b14dda9504cd96cac992bf
connor gammill <connor.gammill@outlook.com>
2024-12-18 00:24:42 -0500

    Add project files.

---
+++ Assignment7.2.3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Assignment7._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Enter the first word:"); 
+			string word1 = Console.ReadLine(); 
+			Console.WriteLine("Enter the second word:"); 
+			string word2 = Console.ReadLine(); 
+			bool result = IsAnagram(word1, word2); 
+			if (result) 
+			{ 
+				Console.WriteLine($"{word1} and {word2} are anagrams."); 
+			} 
+			else 
+			{ 
+				Console.WriteLine($"{word1} and {word2} are not anagrams."); 
+			}
+		}
+		public static bool IsAnagram(string s, string t)
+		{
+			if (s.Length != t.Length)
+			{
+				return false;
+			}
+
+			char[] sSort = s.ToCharArray();
+			char[] tSort = t.ToCharArray();
+			Array.Sort(sSort);
+			Array.Sort(tSort);
+			return sSort.SequenceEqual(tSort);
+		}
+	}
+}
